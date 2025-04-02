62e7dff8ef8bf74726f0f0034b0ed536712264f6
riicardojs <ricardo.jeronymo@hotmail.com>
2025-04-02 00:56:56 -0300

    feat: dependency inversion and global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { env } from 'process';
+import { ZodError } from 'zod';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
-export const prisma = new PrismaClient();
+app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // Todo: external log tool (DataDog/NewRelic/Sentry)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
