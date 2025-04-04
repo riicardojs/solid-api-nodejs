18e0b5d5852aeda0ef3c77931be2a85189f08546
riicardojs <ricardo.jeronymo@hotmail.com>
2025-04-04 20:11:19 -0300

    feat: add prisma check ins repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -6,5 +6,5 @@ export interface CheckInsRepository {
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
-  save(checkIn: CheckIn): Promise<CheckIn>;
+  save(data: CheckIn): Promise<CheckIn>;
 }
