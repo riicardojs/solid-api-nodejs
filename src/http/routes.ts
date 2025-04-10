4b1dbb9394c97a5b141c36be853948903fe70801
riicardojs <ricardo.jeronymo@hotmail.com>
2025-04-10 00:55:17 -0300

    feat: add verifyJwt authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { authenticate } from './controllers/authenticate';
+import { profile } from './controllers/profile';
 import { register } from './controllers/register';
+import { jwtVerify } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  /** Authenticate **/
+  app.get('/me', { onRequest: [jwtVerify] }, profile);
 }
