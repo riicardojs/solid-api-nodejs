ee33d11c30dbfdd0e50c1d014266529feaae634d
riicardojs <ricardo.jeronymo@hotmail.com>
2025-04-15 23:00:06 -0300

    feat: add test e2e authenticate

--- src/http/controllers/authenticate.e2e.test.ts
+++ src/http/controllers/authenticate.e2e.test.ts
@@ -1,5 +1,31 @@
-import { expect, test } from 'vitest';
+import { app } from '@/app';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {
-  expect(1 + 1).toBe(2);
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Ricardo Santos',
+      email: 'ricardo.santos@hotmail.com',
+      password: '123456',
+    });
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'ricardo.santos@hotmail.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
 });
